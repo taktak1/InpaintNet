bb8786532e520909869d71c6afa7fd7038717d5e
tak <tak.tak@kyp.biglobe.ne.jp>
2019-12-26 17:20:57 +0900

    Update metadata.py

--- DatasetManager/metadata.py
+++ DatasetManager/metadata.py
@@ -1,5 +1,5 @@
 import numpy as np
-from music21 import analysis, stream
+from music21 import meter, analysis, stream
 from DatasetManager.helpers import SLUR_SYMBOL, \
     PAD_SYMBOL, BEAT_SYMBOL, DOWNBEAT_SYMBOL
 
